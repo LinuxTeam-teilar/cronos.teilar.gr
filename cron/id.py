41351c7f14d462fa66db8c59b239c38d19d2d55f
Theo Chatzimichos <tampakrap@gentoo.org>
2010-05-15 17:10:05 +0000

    Small fix regarding proj_root.py, it needs to be inside cron/ dir for the non-project cron job file to be able to read it

--- cron/id.py
+++ cron/id.py
@@ -1,5 +1,5 @@
-##### -*- coding: utf-8 -*-
-from cronos.proj_root import *
+# -*- coding: utf-8 -*-
+from proj_root import *
 import sys
 import os
 sys.path.append(PROJ_ROOT)
@@ -121,11 +121,11 @@ for item in soup.findAll('a'):
 
 		lesson = ''
 		for j in xrange(len((soup.findAll('a')[i].contents[0]).split('-')) - 1):
-			lesson = str(soup.findAll('a')[i].contents[0]).split('-')[j+1].strip() + ' '
+			lesson += str(soup.findAll('a')[i].contents[0]).split('-')[j+1].strip() + ' '
 
 		eclass = Id(
 			urlid = str(cid),
-			name = lesson.strip(),
+			name = 'e-class: ' + lesson.strip(),
 			department = '',
 			email = '',
 		)
