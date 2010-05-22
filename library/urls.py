822e131f607436d878d35cd5b87e8a28d789125c
Theo Chatzimichos <tampakrap@gentoo.org>
2010-05-22 02:44:11 +0000

    Remove uneeded things from urls.py, and fix position of the link variable, it should be after the connection and the pass of the search variable

--- library/urls.py
+++ library/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('cronos.library.views',
-	url(r'^$','search',name="search"),
+	url(r'^$','library',name="search"),
 )
--- webmail/urls.py
+++
@@ -1,5 +0,0 @@
-from django.conf.urls.defaults import *
-
-urlpatterns = patterns('cronos.webmail.views',
-	url(r'^$','passed_id',name="passed_id"),
-)
