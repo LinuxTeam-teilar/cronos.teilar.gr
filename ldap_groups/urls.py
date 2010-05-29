a41b69d767a1d671bc15d8504b54cf934c95948f
Theo Chatzimichos <tampakrap@gentoo.org>
2010-05-29 14:51:01 +0000

    Small fixes

--- ldap_groups/urls.py
+++ ldap_groups/urls.py
@@ -1,5 +1,7 @@
+# -*- coding: utf-8 -*-
+
 from django.conf.urls.defaults import *
-from ldap_groups.views import ldap_search
+from cronos.ldap_groups.views import ldap_search
 
 urlpatterns = patterns('',
     url(r'^ldap_search/$', ldap_search, name='ldap_search'),
