8ce57d3b93523569c1e962873068fd83736af107
Theo Chatzimichos <tampakrap@gentoo.org>
2010-05-29 13:55:36 +0000

    UNTESTED COMMIT: add all ldap attributes to profildap model Redesign backends.py using tabs instead of (stupid) spaces Small corrections to settings.py, move ldap base username/password to local_settings

--- profildap/models.py
+++ profildap/models.py
@@ -4,5 +4,18 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class LdapProfile(models.Model):
-	user = models.ForeignKey(User, unique=True)
-	dionysos_username = models.CharField(max_length=100, blank=True)
+	user = models.ForeignKey(User, unique = True)
+	dionysos_username = models.CharField(max_length = 100, unique = True)
+	dionysos_password = models.CharField(max_length = 100)
+	eclass_username = models.CharField(max_length = 100, blank = True)
+	eclass_password = models.CharField(max_length = 100, blank = True)
+	eclass_lessons = models.TextField()
+	introduction_year = models.CharField(max_length = 5)
+	registration_number = models.CharField(max_length = 5)
+	school = models.CharField(max_length = 4)
+	semester = models.CharField(max_length = 3)
+	webmail_username = models.CharField(max_length = 100, blank = True)
+	webmail_password = models.CharField(max_length = 100, blank = True)
+	teacher_announcements = models.TextField(blank = True)
+	other_announcements = models.TextField(blank = True)
+	declaration = models.TextField()
