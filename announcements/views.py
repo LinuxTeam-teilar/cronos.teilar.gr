ea14d459373214dd67a6077083c1f2c47f0ae47c
Theo Chatzimichos <tampakrap@gentoo.org>
2010-06-02 14:51:11 +0000

    Add few more avatar icons, fix the equivalent code that selects the correct one

--- announcements/views.py
+++ announcements/views.py
@@ -9,18 +9,24 @@ from django.http import HttpResponse
 from django.template import Context
 from django.template.loader import get_template
 
-announce = []
+announce_list = []
 i = 0
 
 for item in Announcements.objects.order_by('-date_fetched')[:30]:
-	announce.append([])
+	announce_list.append([])
+	img = item.urlid.urlid
 	if (item.urlid.urlid[:2] == 'CS'):
 		img = 'eclass'
-	else:
+	if (item.urlid.urlid[:3] == 'pid'):
+		img = 'teacher'
+	if (item.urlid.urlid == 'pid323') or (item.urlid.urlid == 'pid324') or (item.urlid.urlid == 'pid326') or (item.urlid.urlid == 'pid327'):
 		img = item.urlid.urlid
+	if (item.urlid.urlid[:3] == 'cid') and (item.urlid.urlid[1:] > 0) and (item.urlid.urlid[1:] < 50):
+		img = 'department'
+	if (item.urlid.urlid[
 	announce1 = [img, item.author(), str(item.date()), str(item.id), item.__unicode__()]
 	for j in xrange(5):
-		announce[i].append(announce1[j][:])
+		announce_list[i].append(announce1[j][:])
 	i += 1
 
 @login_required
@@ -28,12 +34,8 @@ def announcements(request):
 	form = AnnouncementForm(request.GET)
 	if (len(str(request.GET.get('announceid'))) != 4):
 		db = Announcements.objects.filter(id__exact = request.GET.get('announceid'))
-		for item in db:		
-			if (item.urlid.urlid[:2] == 'CS'):
-				img = 'eclass'
-			else:
-				img = item.urlid.urlid
-			announce2 = [img, item.author(), str(item.date()), item.__unicode__(), item.get_absolute_url(), item.body()]
+		for item in db:
+			announce2 = [item.author(), str(item.date()), item.__unicode__(), item.get_absolute_url(), item.body()]
 		template = get_template('announcements.html')
 		variables = Context({
 			'id': 'set',
@@ -44,7 +46,7 @@ def announcements(request):
 	else:
 		template = get_template('announcements.html')
 		variables = Context({
-			'items': announce,
+			'items': announce_list,
 			'MEDIA_URL': settings.MEDIA_URL,
 		})
 		output = template.render(variables)
