d0c534911276649395fe889c1ecb23031f30924b
Ioannis Spanos <spioannis@gmail.com>
2010-06-02 23:50:48 +0000

    add introduction year

--- dionysos/views.py
+++ dionysos/views.py
@@ -40,6 +40,9 @@ soup2 = BeautifulSoup(str(soup1.findAll('tr')[7]))
 am = str(soup2.findAll('td')[1].contents[0])
 soup2 = BeautifulSoup(str(soup1.findAll('tr')[9]))
 eksamino = str(soup2.findAll('td')[1].contents[0])
+soup2 = BeautifulSoup(str(soup.findAll('table')[15]))
+intr_year = str(eisagogi1.findAll('span','tablecell')[0].contents[0].split('-')[0]) + str(souptest.findAll('span','tablecell')[1].contents[0])[:2]
+
 
 
 b = StringIO.StringIO()
