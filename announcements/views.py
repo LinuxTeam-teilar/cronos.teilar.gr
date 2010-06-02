e88be3449fb8c7d9a8f6043afc266b800f520778
Theo Chatzimichos <tampakrap@gentoo.org>
2010-06-02 20:07:34 +0000

    Finish announcements: views small redisign use ContextRequest for easy pass of MEDIA_URL to template (other templates need that too) small fix regarding the previous comment in base.html and urls.py

--- announcements/views.py
+++ announcements/views.py
@@ -6,29 +6,9 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render_to_response
 from django.conf import settings
 from django.http import HttpResponse
-from django.template import Context
-from django.template.loader import get_template
+from django.template import Context,RequestContext, Template
+from django.template.loader import render_to_string
 
-announce_list = []
-i = 0
-
-for item in Announcements.objects.order_by('-date_fetched')[:30]:
-	announce_list.append([])
-	img = item.urlid.urlid
-	if (item.urlid.urlid[:2] == 'CS'):
-		img = 'eclass'
-	if (item.urlid.urlid[:3] == 'pid'):
-		img = 'teacher'
-	if (item.urlid.urlid == 'pid323') or (item.urlid.urlid == 'pid324'):
-		img = item.urlid.urlid
-	if (item.urlid.urlid == 'pid326') or (item.urlid.urlid == 'pid327'):
-		img == 'meeting'
-	if (item.urlid.urlid[:3] == 'cid') and (int(item.urlid.urlid[3:]) > 0) and (int(item.urlid.urlid[3:]) < 50):
-		img = 'department'
-	announce1 = [img, item.author(), str(item.date()), str(item.id), item.__unicode__()]
-	for j in xrange(5):
-		announce_list[i].append(announce1[j][:])
-	i += 1
 
 @login_required
 def announcements(request):
@@ -37,20 +17,32 @@ def announcements(request):
 		db = Announcements.objects.filter(id__exact = request.GET.get('announceid'))
 		for item in db:
 			announce_body = [item.author(), str(item.date()), item.__unicode__(), item.get_absolute_url(), item.body()]
-		template = get_template('announcements.html')
-		variables = Context({
-			'head_title': 'Ανακοινώσεις |',
-			'content': announce_body,
-		})
-		output = template.render(variables)
-		return HttpResponse(output)
+		return render_to_response('announcements.html', {
+				'head_title': 'Ανακοινώσεις | ',
+				'content': announce_body,
+			}, context_instance = RequestContext(request))
 	else:
-		template = get_template('announcements.html')
-		variables = Context({
-			'head_title': 'Ανακοινώσεις |',
-			'items': announce_list,
-			'MEDIA_URL': settings.MEDIA_URL,
-		})
-		output = template.render(variables)
-		return HttpResponse(output)
-	return render_to_response('announcements.html', {'form': form } )
+		announce_list = []
+		i = 0
+		for item in Announcements.objects.order_by('-date_fetched')[:30]:
+			announce_list.append([])
+			img = item.urlid.urlid
+			if (item.urlid.urlid[:2] == 'CS'):
+				img = 'eclass'
+			if (item.urlid.urlid[:3] == 'pid'):
+				img = 'teacher'
+			if (item.urlid.urlid == 'pid323') or (item.urlid.urlid == 'pid324'):
+				img = item.urlid.urlid
+			if (item.urlid.urlid == 'pid326') or (item.urlid.urlid == 'pid327'):
+				img == 'meeting'
+			if (item.urlid.urlid[:3] == 'cid') and (int(item.urlid.urlid[3:]) > 0) and (int(item.urlid.urlid[3:]) < 50):
+				img = 'department'
+			announce1 = [img, item.author(), str(item.date()), str(item.id), item.__unicode__()]
+			for j in xrange(5):
+				announce_list[i].append(announce1[j][:])
+			i += 1
+		return render_to_response('announcements.html', {
+				'head_title': 'Ανακοινώσεις | ',
+				'items': announce_list,
+				'form': form,
+			}, context_instance = RequestContext(request))
--- urls.py
+++ urls.py
@@ -38,7 +38,7 @@ urlpatterns = patterns('',
 
 	(r'^feeds/(?P<url>.*)/$', 'django.contrib.syndication.views.feed', {'feed_dict': feeds}),
 
-	('^' + settings.MEDIA_URL + '/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT }),
+	(r'^site_media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT, 'show_indexes': True }),
 
 	# Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
 	# to INSTALLED_APPS to enable admin documentation:
