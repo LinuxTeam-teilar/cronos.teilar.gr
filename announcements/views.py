166cfc4699a898180f856957b3ab766b05d19d83
Theo Chatzimichos <tampakrap@linuxteam.teilar.gr>
2010-06-05 04:23:46 +0000

    Finish announcements, improve eclass Improve code of cron jobs, no actual changes

--- announcements/views.py
+++ announcements/views.py
@@ -12,18 +12,24 @@ def announcements(request):
 	id = ''
 	if (request.GET.get('announceid')):
 		form = AnnouncementForm(request.GET)
-		db = Announcements.objects.filter(id__exact = request.GET.get('announceid'))
-		for item in db:
-			announce = [item.author(), str(item.date()), item.__unicode__(), item.get_absolute_url(), item.body()]
+		for item in Announcements.objects.filter(id__exact = request.GET.get('announceid')):
+			all_announcements = [item.author(), str(item.date()), item.__unicode__(), item.get_absolute_url(), item.body()]
 		id = request.GET.get('announceid')
 	else:
 		form = AnnouncementForm()
-		announce = []
-		i = 0
-		for item in Announcements.objects.order_by('-date_fetched'):
-			if request.user.get_profile().school == item.urlid.urlid or item.urlid.urlid == 'cid52' or item.urlid.urlid == 'cid55' or item.urlid.urlid == 'cid51': #got to finish it
+		all_announcements = []
+		all_announcements_ids = [request.user.get_profile().school]
+		try:
+			all_announcements_ids += request.user.get_profile().eclass_lessons.split(',')
+			all_announcements_ids += request.user.get_profile().other_announcements.split(',')
+			all_announcements_ids += request.user.get_profile().teacher_announcements.split(',')
+		except:
+			pass
+		print all_announcements_ids
+		for item in Announcements.objects.filter(urlid__urlid__in = all_announcements_ids).order_by('-date_fetched')[:30]:
+				print item
 				img = item.urlid.urlid
-				if (item.urlid.urlid[:2] == 'CS'):
+				if (item.urlid.urlid[:3] != 'pid') and (item.urlid.urlid[:3] != 'cid'):
 					img = 'eclass'
 				if (item.urlid.urlid[:3] == 'pid'):
 					img = 'teacher'
@@ -33,13 +39,10 @@ def announcements(request):
 					img == 'meeting'
 				if (item.urlid.urlid[:3] == 'cid') and (int(item.urlid.urlid[3:]) > 0) and (int(item.urlid.urlid[3:]) < 50):
 					img = 'department'
-				announce.append([img, item.author(), str(item.date()), str(item.id), item.__unicode__()])
-				if i == 30:
-					break
-				i += 1
+				all_announcements.append([img, item.author(), str(item.date()), str(item.id), item.__unicode__()])
 	return render_to_response('announcements.html', {
 			'head_title': 'Ανακοινώσεις | ',
-			'items': announce,
+			'items': all_announcements,
 			'form': form,
 			'id': id,
 		}, context_instance = RequestContext(request))
