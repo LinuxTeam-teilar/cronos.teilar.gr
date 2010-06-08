013933ebb4274ec359e84937a1a203226599d2a3
Theo Chatzimichos <tampakrap@gentoo.org>
2010-06-08 10:46:32 +0000

    Create functions getmail and getschool so they can be used in both sites user and settings

--- user/views.py
+++ user/views.py
@@ -5,25 +5,30 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.contrib.auth.models import User
 from cronos.announcements.models import Id
-	
-if request.user.email[-21:] == 'notapplicablemail.com':
-	mail = 'unset'
-elif request.user.get_profile().webmail_username:
-	mail = webmail_username + '@teilar.gr'
-else:
-	''
-for item in Id.objects.filter(urlid__exact = request.user.get_profile().school):
-	school = str(item.name)
+
+def getmail(request):
+	if request.user.email[-21:] == 'notapplicablemail.com':
+		mail = 'unset'
+	elif request.user.get_profile().webmail_username:
+		mail = webmail_username + '@teilar.gr'
+	else:
+		''
+	return mail
+
+def getschool(request):
+	for item in Id.objects.filter(urlid__exact = request.user.get_profile().school):
+		school = str(item.name)
+	return school
 
 @login_required
 def user(request):
 	return render_to_response('user.html', {
-			'mail': mail,
+			'mail': getmail(request),
 		}, context_instance = RequestContext(request))
 
 @login_required
 def user_settings(request):
 	return render_to_response('settings.html', {
-			'school': school,
-			'mail': mail,
+			'school': getschool(request),
+			'mail': getmail(request),
 		}, context_instance = RequestContext(request))
