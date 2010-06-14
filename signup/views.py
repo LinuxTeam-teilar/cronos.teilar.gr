cdcb5253515465340f08f7bdebe59fb47513d863
Theo Chatzimichos <tampakrap@gentoo.org>
2010-06-14 13:42:39 +0000

    Modularization: Create a teilar module with functions that perform login in eclas,dionysos,webmail and return the output With this the code gets reduced a loooooooooooooooot

--- signup/views.py
+++ signup/views.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 
+from BeautifulSoup import BeautifulSoup
 from cronos.signup.forms import *
 from cronos.announcements.models import Id
+from cronos.login.teilar import *
 from django.conf import settings
 from django.template import RequestContext
 from django.shortcuts import render_to_response
@@ -40,41 +42,11 @@ class SignupWizard(FormWizard):
 				from random import choice
 				import string
 				dionysos_password = ''.join([choice(string.printable) for i in range(20)])
-				msg = 'Ο κωδικός δεν επαληθεύτηκε'
+				msg = 'Οι κωδικοί δεν ταιριάζουν'
 			else:
 				password = Sha1Password(password1)
-			from BeautifulSoup import BeautifulSoup
-			import pycurl
-			import StringIO
-			import urllib
-			import os
-			import urlparse
-
-			conn = pycurl.Curl()
-
-			# login to dionysos
-			b = StringIO.StringIO()
-			conn = pycurl.Curl()
-			cookie_file_name = os.tempnam('/tmp','dionysos')
-			login_form_seq = [
-				('userName', dionysos_username),
-				('pwd', base64.b64decode(dionysos_password)),
-				('submit1', '%C5%DF%F3%EF%E4%EF%F2'),
-				('loginTrue', 'login')
-			]
-			login_form_data = urllib.urlencode(login_form_seq)
-			conn.setopt(pycurl.FOLLOWLOCATION, 1)
-			conn.setopt(pycurl.COOKIEFILE, cookie_file_name)
-			conn.setopt(pycurl.COOKIEJAR, cookie_file_name)
-			conn.setopt(pycurl.URL, 'http://dionysos.teilar.gr/unistudent/')
-			conn.setopt(pycurl.POST, 0)
-			conn.perform()
-			conn.setopt(pycurl.URL, 'http://dionysos.teilar.gr/unistudent/login.asp')
-			conn.setopt(pycurl.POST, 1)
-			conn.setopt(pycurl.POSTFIELDS, login_form_data)
-			conn.setopt(pycurl.WRITEFUNCTION, b.write)
-			conn.perform()
-			output = (b.getvalue()).decode('windows-1253')
+	
+			output = dionysos_login(0, dionysos_username, base64.b64decode(dionysos_password))
 			soup = BeautifulSoup(output)
 			soup1 = BeautifulSoup(str(soup.findAll('table')[13]))
 			soup2 = BeautifulSoup(str(soup1.findAll('tr')[5]))
@@ -97,14 +69,7 @@ class SignupWizard(FormWizard):
 				year = str(soup2.findAll('span','tablecell')[0].contents[0].split('-')[0])
 			introduction_year = year + season
 			try:
-				b = StringIO.StringIO()
-				conn.setopt(pycurl.URL, 'http://dionysos.teilar.gr/unistudent/stud_NewClass.asp?studPg=1&mnuid=diloseis;newDil&')
-				conn.setopt(pycurl.POST, 1)
-				conn.setopt(pycurl.POSTFIELDS, login_form_data)
-				conn.setopt(pycurl.COOKIE, cookie_file_name)
-				conn.setopt(pycurl.WRITEFUNCTION, b.write)
-				conn.perform()
-				output = (b.getvalue()).decode('windows-1253')
+				output = dionysos_login('declaration', '', '')
 				soup = BeautifulSoup(output)
 				soup1 = BeautifulSoup(str(soup.findAll('table')[14]))
 
@@ -129,19 +94,7 @@ class SignupWizard(FormWizard):
 
 			# login to eclass
 			if eclass_username:
-				b = StringIO.StringIO()
-				login_form_seq = [
-					('uname', eclass_username),
-					('pass', base64.b64decode(eclass_password)),
-					('submit', 'E%95%CE%AF%CF%83%CE%BF%CE%B4%CE%BF%CF%82')
-				]
-				login_form_data = urllib.urlencode(login_form_seq)
-				conn.setopt(pycurl.FOLLOWLOCATION, 1)
-				conn.setopt(pycurl.POSTFIELDS, login_form_data)
-				conn.setopt(pycurl.URL, 'http://e-class.teilar.gr/index.php')
-				conn.setopt(pycurl.WRITEFUNCTION, b.write)
-				conn.perform()
-				output = unicode(b.getvalue(), 'utf-8', 'ignore')
+				output = eclass_login(eclass_username, base64.b64decode(eclass_password))
 				soup = BeautifulSoup(output).find('table', 'FormData')
 				i = 0
 				eclass_lessons = []
@@ -152,28 +105,7 @@ class SignupWizard(FormWizard):
 
 			# login to webmail
 			if webmail_username:
-				b = StringIO.StringIO()
-				cookie_file_name = os.tempnam('/tmp','webmail')
-				login_form_seq = [
-					('login_username', webmail_username),
-					('secretkey', base64.b64decode(webmail_password)),
-					('js_autodetect_results', '1'),
-					('just_logged_in', '1')
-				]
-				login_form_data = urllib.urlencode(login_form_seq)
-				conn.setopt(pycurl.FOLLOWLOCATION, 0)
-				conn.setopt(pycurl.COOKIEFILE, cookie_file_name)
-				conn.setopt(pycurl.COOKIEJAR, cookie_file_name)
-				conn.setopt(pycurl.URL, 'http://myweb.teilar.gr')
-				conn.setopt(pycurl.POST, 0)
-				conn.perform()
-				conn.setopt(pycurl.URL, 'http://myweb.teilar.gr/src/redirect.php')
-				conn.setopt(pycurl.POST, 1)
-				conn.setopt(pycurl.POSTFIELDS, login_form_data)
-				conn.setopt(pycurl.WRITEFUNCTION, b.write)
-				conn.perform()
-				conn.setopt(pycurl.URL, 'http://myweb.teilar.gr/src/right_main.php')
-				conn.perform()
+				webmail_login(0, webmail_username, base64.b64decode(webmail_password))
 
 			# add to ldap
 			from django.conf import settings
