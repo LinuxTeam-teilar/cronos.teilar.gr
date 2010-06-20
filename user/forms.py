33b5bac8b82b99d11e9964dfa11569bb15f17faf
Theo Chatzimichos <tampakrap@linuxteam.teilar.gr>
2010-06-20 18:05:25 +0000

    Select boxes work with a little bug, to be fixed

--- user/forms.py
+++ user/forms.py
@@ -6,21 +6,6 @@ from django import forms
 from django.db.models import Q
 
 
-'''class TeacherAnnouncementsForm(forms.ModelForm):
-	teacher_announcements = forms.ModelMultipleChoiceField(queryset = Id.objects.filter(urlid__startswith = 'pid').order_by('name'), widget = forms.SelectMultiple)
-
-	class Meta:
-		model = LdapProfile
-		exclude = ['user', 'declaration', 'dionysos_username', 'dionysos_password', 'eclass_username', 'eclass_password', 'eclass_lessons', 'introduction_year', 'registration_number', 'school', 'semester', 'webmail_username', 'webmail_password', 'teacher_announcements', 'other_announcements']
-		fields = ['teacher_announcements']
-
-class OtherAnnouncementsForm(forms.ModelForm):
-	other_announcements = forms.ModelMultipleChoiceField(queryset = Id.objects.filter(Q(urlid__startswith = 'cid5') | Q(urlid__exact = 'cid0')).order_by('name'), widget = MultipleSelectWithPop)
-
-	class Meta:
-		model = LdapProfile
-		exclude = ['user', 'declaration', 'dionysos_username', 'dionysos_password', 'eclass_username', 'eclass_password', 'eclass_lessons', 'introduction_year', 'registration_number', 'school', 'semester', 'webmail_username', 'webmail_password', 'teacher_announcements', 'other_announcements']'''
-
 class CronosForm(forms.Form):
 	old_password = forms.CharField(max_length = 30, widget = forms.PasswordInput(), label = 'Παλιός Κωδικός:')
 	password1 = forms.CharField(max_length = 30, widget = forms.PasswordInput(), label = 'Νέος Κωδικός:')
@@ -49,3 +34,20 @@ class GradesForm(forms.Form):
 
 class Eclass2Form(forms.Form):
 	eclass_lessons = forms.CharField(widget = forms.HiddenInput())
+
+# this won't be used, but it was a good piece of code, leaving it here for historical reasons
+'''class TeacherAnnouncementsForm(forms.ModelForm):
+	teacher_announcements = forms.ModelMultipleChoiceField(queryset = Id.objects.filter(urlid__startswith = 'pid').order_by('name'), widget = forms.SelectMultiple)
+
+	class Meta:
+		model = LdapProfile
+		exclude = ['user', 'declaration', 'dionysos_username', 'dionysos_password', 'eclass_username', 'eclass_password', 'eclass_lessons', 'introduction_year', 'registration_number', 'school', 'semester', 'webmail_username', 'webmail_password', 'teacher_announcements', 'other_announcements']
+		fields = ['teacher_announcements']
+
+class OtherAnnouncementsForm(forms.ModelForm):
+	other_announcements = forms.ModelMultipleChoiceField(queryset = Id.objects.filter(Q(urlid__startswith = 'cid5') | Q(urlid__exact = 'cid0')).order_by('name'), widget = MultipleSelectWithPop)
+
+	class Meta:
+		model = LdapProfile
+		exclude = ['user', 'declaration', 'dionysos_username', 'dionysos_password', 'eclass_username', 'eclass_password', 'eclass_lessons', 'introduction_year', 'registration_number', 'school', 'semester', 'webmail_username', 'webmail_password', 'teacher_announcements', 'other_announcements']'''
+
