98dfc5569fb1ae58905f8b6a36deeda324dcdd7b
Theo Chatzimichos <tampakrap@linuxteam.teilar.gr>
2012-04-21 21:27:18 +0200

    Add deprecated flag for teachers and departments

--- cronos/teilar/models.py
+++ cronos/teilar/models.py
@@ -3,12 +3,17 @@ from django.db import models
 class Departments(models.Model):
     urlid = models.IntegerField(unique = True)
     name = models.CharField("Department name", max_length = 200)
+    deprecated = models.BooleanField(default = False)
+
+    def __unicode__(self):
+        return self.name
 
 class Teachers(models.Model):
-    urlid = models.CharField("URL ID", max_length = 30, unique = True)
+    urlid = models.IntegerField(unique = True)
     name = models.CharField("Teacher name", max_length = 100)
     email = models.EmailField("Teacher's mail", null = True)
     department = models.CharField("Teacher's department", max_length = 100, null = True)
+    deprecated = models.BooleanField(default = False)
 
     def __unicode__(self):
         return self.name
