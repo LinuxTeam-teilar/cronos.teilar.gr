2a184635f62b61a5ec7a2c312d497dd3aa057a31
Theo Chatzimichos <tampakrap@linuxteam.teilar.gr>
2012-07-09 11:56:39 +0200

    department field in Teacher table is now a foreign key of the Department table

--- cronos/teilar/models.py
+++ cronos/teilar/models.py
@@ -12,7 +12,7 @@ class Teachers(models.Model):
     urlid = models.IntegerField(unique = True)
     name = models.CharField("Teacher name", max_length = 100)
     email = models.EmailField("Teacher's mail", null = True)
-    department = models.CharField("Teacher's department", max_length = 100, null = True)
+    department = models.ForeignKey(Departments)
     deprecated = models.BooleanField(default = False)
 
     def __unicode__(self):
