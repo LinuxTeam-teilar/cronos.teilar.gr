9a60772e998d66c6d87e6461257f6405d801fd35
Theo Chatzimichos <tampakrap@linuxteam.teilar.gr>
2012-07-11 02:18:45 +0200

    New script/cronjob that pulls the teachers from teilar.gr and puts them in the DB. Also, it can update their attributes in case there are changes

---
+++ cronos/teilar/teachers_data_get.py
@@ -0,0 +1,175 @@
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+from proj_root import PROJECT_ROOT
+sys.path.append(PROJECT_ROOT)
+os.environ['DJANGO_SETTINGS_MODULE'] = 'cronos.settings'
+from bs4 import BeautifulSoup
+from cronos.log import CronosError, log_extra_data
+from cronos.teilar.models import Departments, Teachers
+from cronos.teilar.websites_login import teilar_login
+import logging
+import StringIO
+import pycurl
+
+logger_syslog = logging.getLogger('cronos')
+logger_mail = logging.getLogger('mail_cronos')
+
+def get_teachers():
+    '''
+    Retrieves the teachers from teilar.gr
+    The output is dictionary with the following structure:
+    teachers_from_teilar = { teacher_id: ['name', 'email', 'department'] }
+    '''
+    teachers_from_teilar = {}
+    conn = pycurl.Curl()
+    for pid in range(400):
+        '''
+        Perform connections to each of the teacher's profile page. From the HTML
+        output we grab the name, email and department
+        '''
+        b = StringIO.StringIO()
+        output = teilar_login('http://www.teilar.gr/person.php?pid=' + str(pid))
+        soup = BeautifulSoup(output)
+        name = None
+        email = None
+        department = None
+        try:
+            name = soup.findAll('td', 'BlackText11Bold')[1].contents[0].strip()
+        except IndexError:
+            '''
+            No teacher found, continue with the next item of the loop
+            '''
+            continue
+        try:
+            email = soup.findAll('td', 'BlackText11')[5].a.contents[0].split(' ')[0].strip()
+        except AttributeError:
+            try:
+                email = soup.findAll('td', 'BlackText11')[5].contents[0].strip()
+            except IndexError:
+                pass
+        except IndexError:
+            pass
+        try:
+            '''
+            The string replace in the end is to keep it in track with dionysos.teilar.gr
+            '''
+            department = soup.findAll('td', 'BlackText11')[2].contents[0].strip().replace(u'Τεχν.', u'Τεχνολογίας')
+        except IndexError:
+            pass
+        teachers_from_teilar[pid] = [name, email, department]
+    return teachers_from_teilar
+
+def add_teacher_to_db(teacher_id, attributes):
+    try:
+        teachers = Teachers(
+            urlid = teacher_id,
+            name = attributes[0],
+            email = attributes[1],
+            department = Departments.objects.get(name = attributes[2])
+        )
+        teachers.save()
+        status = u'Ο/Η %s προστέθηκε επιτυχώς' % (attributes[0])
+        logger_syslog.info(status, extra = log_extra_data())
+    except Exception as error:
+        logger_syslog.error(error, extra = log_extra_data(cronjob = attributes[0]))
+        logger_mail.exception(error)
+        raise CronosError(u'Παρουσιάστηκε σφάλμα κατά την προσθήκη του %s' % (attributes[0]))
+    return
+
+def deprecate_teacher_in_db(teacher_id):
+    '''
+    Mark teachers as deprecated
+    '''
+    teacher = Teachers.objects.get(urlid = teacher_id)
+    teacher.deprecated = True
+    try:
+        teacher.save()
+        status = u'Αλλαγή κατάστασης σε deprecated'
+        logger_syslog.info(status, extra = log_extra_data(cronjob = teacher.name))
+    except Exception as error:
+        logger_syslog.error(error, extra = log_extra_data(cronjob = teacher.name))
+        logger_mail.exception(error)
+        raise CronosError(u'Παρουσιάστηκε σφάλμα κατά την αλλαγή κατάστασης σε deprecated')
+    return
+
+def update_teachers():
+    '''
+    1) Find teachers that left the school mark them as deprecated
+    2) Find new teachers and add them
+    3) In the existing teachers, find changes in their attributes and
+    update them accordingly
+    '''
+    teachers_from_teilar = get_teachers()
+    '''
+    Get all the teachers from the DB and put them in a dictionary in the structure:
+    teachers_from_db = { teacher_id: ['name', 'email', 'department'] }
+    '''
+    teachers_from_db = {}
+    teachers_from_db_q = Teachers.objects.filter(deprecated = False)
+    for teacher in teachers_from_db_q:
+        teachers_from_db[teacher.urlid] = [teacher.name, teacher.email, teacher.department]
+    '''
+    Get the teacher_IDs in set data structure, for easier comparisons
+    '''
+    teachers_from_teilar_ids = set(teachers_from_teilar.keys())
+    try:
+        teachers_from_db_ids = set(teachers_from_db.keys())
+    except AttributeError:
+        '''
+        Teachers table is empty in the DB
+        '''
+        teachers_from_db_ids = set()
+    '''
+    Get ex teachers and mark them as deprecated
+    '''
+    ex_teachers = teachers_from_db_ids - teachers_from_teilar_ids
+    for teacher_id in ex_teachers:
+        deprecate_teacher_in_db(teacher_id)
+    '''
+    Get new teachers and add them to the DB
+    '''
+    new_teachers = teachers_from_teilar_ids - teachers_from_db_ids
+    for teacher_id in new_teachers:
+        add_teacher_to_db(teacher_id, teachers_from_teilar[teacher_id])
+    '''
+    Get all the existing teachers, and check if any of their attributes were updated
+    '''
+    existing_teachers = teachers_from_teilar_ids & teachers_from_db_ids
+    for teacher_id in existing_teachers:
+        i = 0
+        teacher = Teachers.objects.get(urlid = teacher_id)
+        for attribute in teachers_from_teilar[teacher_id]:
+            if teachers_from_db[teacher_id][i] != attribute:
+                if i == 0:
+                    attr_name = u'name'
+                    teacher.name = attribute
+                elif i == 1:
+                    attr_name = u'email'
+                    teacher.email = attribute
+                elif i == 2:
+                    '''
+                    The department is saved as a foreign key of the
+                    Departments table, thus it needs an extra check
+                    '''
+                    if attribute.strip() == unicode(teacher.department).strip():
+                        continue
+                    attr_name = u'department'
+                    teacher.department = Departments.objects.get(name = attribute)
+                try:
+                    teacher.save()
+                    status = u'Επιτυχής ανανέωση του %s σε %s' % (attr_name, attribute)
+                    logger_syslog.info(status, extra = log_extra_data(cronjob = teacher.name))
+                except Exception as error:
+                    logger_syslog.error(error, extra = log_extra_data(cronjob = teacher.name))
+                    logger_mail.exception(error)
+                    raise CronosError(u'Παρουσιάστηκε σφάλμα κατά την αλλαγή κατάστασης του %s σε %s' % (attr_name, attribute))
+            i += 1
+    return
+
+if __name__ == '__main__':
+    try:
+        update_teachers()
+    except CronosError as error:
+        logger_syslog.error(error.value, extra = log_extra_data())
