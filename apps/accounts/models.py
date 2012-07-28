87a07437c2481f92286f01f988405b4f3cfc5d37
Theo Chatzimichos <tampakrap@linuxteam.teilar.gr>
2012-07-28 23:51:46 +0200

    Make school field foreign key of Departments table

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -1,3 +1,4 @@
+from apps.teilar.models import Departments
 from django.contrib.auth.models import User
 from django.db import models
 
@@ -10,7 +11,7 @@ class UserProfile(models.Model):
     eclass_lessons = models.TextField(null = True, blank = True)
     introduction_year = models.CharField(max_length = 5)
     registration_number = models.CharField(max_length = 8)
-    school = models.CharField(max_length = 5)
+    school = models.ForeignKey(Departments)
     semester = models.CharField(max_length = 2)
     webmail_username = models.CharField(max_length = 30, null = True, blank = True)
     webmail_password = models.CharField(max_length = 30, null = True, blank = True)
