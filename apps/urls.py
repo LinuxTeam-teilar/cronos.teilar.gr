f2e4e7114c61550e5ff6cd690c7a60d71de74ad4
Theo Chatzimichos <tampakrap@linuxteam.teilar.gr>
2012-07-29 00:22:57 +0200

    Disable feeds/announcements temporarily, it should be back alive now

--- apps/urls.py
+++ apps/urls.py
@@ -1,29 +1,29 @@
 # -*- coding: utf-8 -*-
 
+#from apps.announcements.feeds import AnnouncementsFeed
 from django.conf.urls.defaults import patterns, include, url
 from django.conf import settings
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from cronos.announcements.feeds import AnnouncementsFeed
 
-feeds = {
-    'announcements': AnnouncementsFeed,
-}
+#feeds = {
+#    'announcements': AnnouncementsFeed,
+#}
 
-handler500 = 'cronos.login.views.server_error'
+handler500 = 'apps.login.views.server_error'
 
 urlpatterns = patterns('',
-    (r'^$', 'cronos.accounts.views.index'),
-    (r'^about/', 'cronos.accounts.views.about'),
-    (r'^announcements/', 'cronos.announcements.views.announcements'),
-    (r'^dionysos/', 'cronos.dionysos.views.dionysos'),
-    (r'^eclass/', 'cronos.eclass.views.eclass'),
-    (r'^feeds/(?P<url>.*)/$', 'django.contrib.syndication.views.feed', {'feed_dict': feeds}),
-    (r'^library/', 'cronos.library.views.library'),
-    (r'^login/', 'cronos.login.views.cronos_login'),
+    (r'^$', 'apps.accounts.views.index'),
+    (r'^about/', 'apps.accounts.views.about'),
+#    (r'^announcements/', 'apps.announcements.views.announcements'),
+    (r'^dionysos/', 'apps.dionysos.views.dionysos'),
+    (r'^eclass/', 'apps.eclass.views.eclass'),
+#    (r'^feeds/(?P<url>.*)/$', 'django.contrib.syndication.views.feed', {'feed_dict': feeds}),
+    (r'^library/', 'apps.library.views.library'),
+    (r'^login/', 'apps.login.views.cronos_login'),
     (r'^logout/$', 'django.contrib.auth.views.logout', {'next_page': '/login'}),
-    (r'^preferences/', 'cronos.accounts.views.accounts_settings'),
-    (r'^refrigerators/', 'cronos.refrigerators.views.refrigerators'),
-    (r'^teachers/', 'cronos.teachers.views.teachers'),
+    (r'^preferences/', 'apps.accounts.views.accounts_settings'),
+    (r'^refrigerators/', 'apps.refrigerators.views.refrigerators'),
+    (r'^teachers/', 'apps.teachers.views.teachers'),
 )
 
 urlpatterns += staticfiles_urlpatterns()
