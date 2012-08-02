aacf8e219476056bbf3f2d660560aa64d773dcf0
Theo Chatzimichos <tampakrap@linuxteam.teilar.gr>
2012-08-02 02:30:30 +0200

    Update the Announcements model and add in installed_apps

--- apps/announcements/models.py
+++ apps/announcements/models.py
@@ -4,32 +4,12 @@ from django.db import models
 
 class Announcements(models.Model):
     title = models.CharField("Title", max_length = 500)
-    pubdate = models.DateTimeField(auto_now = True)
-    urlid = models.ForeignKey(Id)
-    unique = models.CharField("Unique entry", max_length = 250, unique = True)
-    url = models.URLField()
-    description = models.TextField("Attachment Text", null = True)
-    attachment_text = models.CharField("Attachment Text", max_length = 200, null = True)
-    attachment_url = models.CharField("Attachment URL", max_length = 200, null = True)
-
-    def date(self):
-        return self.date_fetched
-
-    def author(self):
-        return self.urlid.name
-
-    def get_absolute_url(self):
-        return self.url
+    pubdate = models.DateTimeField()
+    author = models.CharField(max_length = 100)
+    unique = models.CharField(max_length = 250, unique = True)
+    link = models.URLField()
+    summary = models.TextField(null = True)
+    enclosure = models.CharField("Attachment URL", max_length = 200, null = True)
 
     def __unicode__(self):
         return self.title
-
-    def body(self):
-        if (len(self.attachment_url) > 1):
-            return u'%s<br /><br /><a href="%s">%s</a><br /><br /> \
-                    <a href="%s">Περισσότερα</a>' % (
-                            self.description,
-                            self.attachment_url,
-                            self.attachment_text, self.url)
-        else:
-            return u'%s<br /><br /><a href="%s">Περισσότερα</a>' % (self.description, self.url)
--- apps/settings.py
+++ apps/settings.py
@@ -112,6 +112,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apps.announcements',
     'apps.accounts',
     'apps.eclass',
     'apps.teilar',
