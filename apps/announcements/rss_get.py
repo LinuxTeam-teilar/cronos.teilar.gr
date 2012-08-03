eda6c540367f9effe8c173096104aac65d248ff3
Theo Chatzimichos <tampakrap@linuxteam.teilar.gr>
2012-08-03 19:20:16 +0200

    1) creator -> author move, also add the hardcoded creators from rss_create to the sites dictionary 2) Add RSS parsing feature, cleanup from no-break spaces (in case of     failure) and add each item to DB 3) Ignore already inserted announcements by checking the unique field

--- apps/announcements/rss_get.py
+++ apps/announcements/rss_get.py
@@ -10,9 +10,12 @@ from apps.announcements.models import Announcements
 from apps.teilar.models import Departments
 from apps.eclass.models import Lessons
 from django.conf import settings
+from django.db.utils import IntegrityError
+from xml.sax._exceptions import SAXException
 import MySQLdb
 import feedparser
 import logging
+import urllib2
 
 logger_syslog = logging.getLogger('cronos')
 logger_mail = logging.getLogger('mail_cronos')
@@ -21,11 +24,11 @@ def get_sites():
     '''
     Retrieves the sites that offer RSS, both from local files
     and from the DB
-    The sites dictionary has the author's name as key, and the
+    The sites dictionary has the creator's name as key, and the
     URL or path as value.
     If we want to use the <dc:creator> tag of the RSS instead
-    of the author written in key value, then the author in the
-    key gets the dummy name "DC:CREATOR"
+    of the creator written in key value, then the creator in the
+    key gets the suffix "_dummy"
     '''
     sites = {
         ## Remote RSS files ##
@@ -38,10 +41,11 @@ def get_sites():
         # TODO: cronos!
         # TODO: diogenis!
         ## Custom made RSS files ##
-        u'general_dummy': u'%s/%s' % (settings.RSS_PATH, 'general.rss'),
-        u'council_dummy': u'%s/%s' % (settings.RSS_PATH, 'council.rss'),
-        u'general_dummy': u'%s/%s' % (settings.RSS_PATH, 'general.rss'),
-        u'teilar_ann_dummy': u'%s/%s' % (settings.RSS_PATH, 'teilar_ann_dummy.rss'),
+        u'Γενικές Ανακοινώσεις': u'%s/%s' % (settings.RSS_PATH, 'general.rss'),
+        u'Ανακοινώσεις του ΤΕΙ Λάρισας': u'%s/%s' % (settings.RSS_PATH, 'teilar_ann_dummy.rss'),
+        u'Συνεδριάσεις Συμβουλίου ΤΕΙ Λάρισας': u'%s/%s' % (settings.RSS_PATH, 'council.rss'),
+        u'Ανακοινώσεις της Επιτροπής Εκπαίδευσης και Ερευνών του ΤΕΙ Λάρισας': u'%s/%s' % (settings.RSS_PATH, 'committee.rss'),
+        u'departments_dummy': u'%s/%s' % (settings.RSS_PATH, 'departments.rss'),
         u'teachers_dummy': u'%s/%s' % (settings.RSS_PATH, 'teachers.rss'),
         #u'Τμήμα Διοίκησης και Διαχείρισης έργων': u'%s/%s' % (settings.RSS_PATH, 'dde.rss'),
         #u'Γραμματεία ΤΕΙ Λάρισας': u'%s/%s' % (settings.RSS_PATH, 'dionysos.rss'),
@@ -78,42 +82,78 @@ def get_announcements():
     sites = get_sites()
 
     '''
-    Parse the RSS feed and grab the tags that are of interest
+    Parse the RSS feed
     '''
-    for author, site in sites.iteritems():
-        print '\n\n######'
-        print author
-        print site
-        rss = feedparser.parse(site)
+    for creator, site in sites.iteritems():
+        try:
+            rss = feedparser.parse(site)
+        except SAXException as warning:
+            '''
+            Bad characters where found, trying to fix
+            '''
+            logger_syslog.warning(warning, extra = log_extra_data(cronjob = site))
+            response = urllib2.urlopen(site)
+            output = response.read()
+            '''
+            Clean no-break space
+            '''
+            output = output.replace('\xa0 ', ' ').replace('\xa0 ', ' ').replace('\xc2 ', '\xc2\xa0 ').replace('\xc2\xa0', '')
+            try:
+                rss = feedparser.parse(output)
+                logger_syslog.info('FIXED!', extra = log_extra_data(cronjob = site))
+            except SAXException as error:
+                '''
+                Still no success, giving up
+                '''
+                # TODO: Try to grab the <item>s with bs4 and create a custom RSS feed
+                logger_syslog.error(error, extra = log_extra_data(cronjob = site))
+                logger_mail.exception(error)
+                continue
+        '''
+        We add only the 10 latest announcements
+        '''
         if len(rss.entries) > 10:
             entries = rss.entries[:10][::-1]
         else:
             entries = rss.entries[::-1]
         for entry in entries:
+            '''
+            Grab the tags that are of interest
+            '''
             title = entry.title
             link = entry.link
             pubdate = entry.updated_parsed
             summary = entry.summary
-            if author.endswith(u'_dummy'):
-                author = entry.author
+            '''
+            Select either the creator of the RSS or
+            the creator from the value of the key of
+            the 'sites' dictionary
+            '''
+            if creator.endswith(u'_dummy'):
+                creator = entry.author
             try:
                 enclosure = entry.enclosures[0].href
             except:
                 enclosure = None
             if site.endswith(u'teachers.rss'):
+                '''
+                Teachers have all the announcements in a
+                single page, instead of having each one
+                in its own page, thus the unique field
+                has to be combined with something else
+                '''
                 unique = link + summary
                 if enclosure:
                     unique += enclosure
             else:
                 unique = link
-            announcement = [title, link, pubdate, summary, author, enclosure, unique]
+            announcement = [title, link, pubdate, summary, creator, enclosure, unique]
             add_announcement_to_db(announcement)
 
 def add_announcement_to_db(announcement):
     '''
     Add the announcement to the DB
     '''
-    # TODO: check first that the announcement is not there using the "unique field"
     # TODO: Fix pubdate
     # TODO: create an announcements_authors table that will connect authors from various tables with the announcements table
     try:
@@ -123,20 +163,28 @@ def add_announcement_to_db(announcement):
             #pubdate = announcement[2],
             pubdate = None,
             summary = announcement[3],
-            author = announcement[4],
+            creator = announcement[4],
             enclosure = announcement[5],
             unique = announcement[6],
         )
         status = u'Νέα ανακοίνωση'
         new_announcement.save()
         logger_syslog.info(status, extra = log_extra_data(cronjob = announcement[0]))
+    except IntegrityError as error:
+        if tuple(error)[0] == 1062 and tuple(error)[1].endswith("'unique'"):
+            '''
+            If the error is a duplicate entry for unique, then silently move forward
+            '''
+            pass
+        else:
+            logger_syslog.error(error, extra = log_extra_data(cronjob= announcement[0]))
+            logger_mail.exception(error)
     except MySQLdb.Warning as warning:
         logger_syslog.info(status, extra = log_extra_data(cronjob = announcement[0]))
         logger_syslog.warning(warning, extra = log_extra_data(cronjob = announcement[0]))
     except Exception as error:
         logger_syslog.error(error, extra = log_extra_data(cronjob = announcement[0]))
         logger_mail.exception(error)
-        raise CronosError(u'Παρουσιάστηκε σφάλμα κατά την προσθήκη ανακοίνωσης στη βάση δεδομένων')
     return
 
 if __name__ == '__main__':
