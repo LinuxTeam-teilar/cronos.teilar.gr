0e1461fe3ab65c1185e644c40bf3c92489886825
Theo Chatzimichos <tampakrap@linuxteam.teilar.gr>
2012-08-12 19:38:53 +0200

    Revert "Temporarily set pubdate default to Null, until I implement proper" it is implemented now This reverts commit 8061905bbd6333276d57af25c8033ab78513a2c2.

--- apps/announcements/models.py
+++ apps/announcements/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Announcements(models.Model):
     title = models.CharField("Title", max_length = 500)
-    pubdate = models.DateTimeField(null = True)
+    pubdate = models.DateTimeField()
     creator = models.CharField(max_length = 200)
     unique = models.CharField(max_length = 250, unique = True)
     link = models.URLField()
