424d919ba30fab5a84d7beba2086a26fd3490893
Theo Chatzimichos <tampakrap@linuxteam.teilar.gr>
2012-08-25 23:53:56 +0200

    Add script that creates logfiles and logdirs and adjusts their perms

---
+++ others/logs_create_fix_perms.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# creates the logdirs and logfiles, and adjusts their permissions
+
+for instance in cronos cronos-dev; do
+    logpath="/var/log/${instance}"
+    [[ -d ${logpath} ]] || mkdir ${logpath}
+    for level in info warning error critical; do
+        logfile=${logpath}/${level}.log
+        [[ -n ${logfile} ]] && touch ${logfile}
+    done
+    chmod -R 640 ${logpath}
+    chown -R root:${instance} ${logpath}
+done
