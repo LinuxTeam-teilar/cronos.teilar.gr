c7633f812ad841ff6d977e74eacf40e4c5d193e6
Theo Chatzimichos <tampakrap@linuxteam.teilar.gr>
2012-08-28 03:40:49 +0200

    Move {img,css,js} from topdir to cronos/static and change static collector accordingly

--- cronos/settings.py
+++ cronos/settings.py
@@ -51,7 +51,7 @@ MEDIA_URL = '/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
-# in cronos' "static/" subdirectories and in STATICFILES_DIRS.
+# under cronos/static/ subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
 STATIC_ROOT = PROJECT_ROOT + '/static/'
 
@@ -64,9 +64,9 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    ('img', PROJECT_ROOT + '/img/'),
-    ('css', PROJECT_ROOT + '/css/'),
-    ('js', PROJECT_ROOT + '/js/'),
+    ('img', PROJECT_ROOT + '/cronos/static/img/'),
+    ('css', PROJECT_ROOT + '/cronos/static/css/'),
+    ('js', PROJECT_ROOT + '/cronos/static/js/'),
     PROJECT_ROOT + '/others/',
 )
 # List of finder classes that know how to find static files in
