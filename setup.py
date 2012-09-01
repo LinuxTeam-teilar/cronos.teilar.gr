98b59ff1329dc6eadad1e898c028a5a4532254f4
Theo Chatzimichos <tampakrap@linuxteam.teilar.gr>
2012-09-01 03:18:20 +0200

    Add local_settings.py.sample in the included files for installation, minor reformatting in setup.py lists of files

--- setup.py
+++ setup.py
@@ -16,14 +16,18 @@ personal data for students of TEI of Larissa',
     include_package_data=True,
     data_files=[
         ('', ['LICENSE', 'manage.py']),
-        ('bin', ['bin/update_cronos.sh']),
-        ('bin', ['bin/logs_create_fix_perms.sh']),
-        ('bin', ['bin/get_full_production_db.sh']),
-        ('configs', ['configs/apache.conf']),
-        ('configs', ['configs/cron.d_cronos']),
-        ('configs', ['configs/logrotate.d_cronos']),
-        ('configs', ['configs/logrotate.d_cronos-dev']),
-        ('configs', ['configs/syslog-ng.conf']),
+        ('bin', [
+            'bin/update_cronos.sh',
+            'bin/logs_create_fix_perms.sh',
+            'bin/get_full_production_db.sh'
+        ]),
+        ('configs', [
+            'configs/apache.conf',
+            'configs/cron.d_cronos',
+            'configs/logrotate.d_cronos',
+            'configs/logrotate.d_cronos-dev',
+            'configs/syslog-ng.conf'
+        ]),
     ],
     cmdclass={'test': test},
 )
