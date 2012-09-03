5839e1e551df96b3766722d8a87d42b12b3cfa5d
Theo Chatzimichos <tampakrap@linuxteam.teilar.gr>
2012-09-04 01:44:36 +0200

    Improve db relations: - User->UserProfile is One to One - UserProfile <-> Teachers/Websites/EclassLessons are Many to Many

--- cronos/accounts/models.py
+++ cronos/accounts/models.py
@@ -1,22 +1,22 @@
-from cronos.teilar.models import Departments
+from cronos.teilar.models import Departments, Teachers, EclassLessons, Websites
 from django.contrib.auth.models import User
 from django.db import models
 
 class UserProfile(models.Model):
-    user = models.ForeignKey(User, unique = True)
+    user = models.OneToOneField(User, primary_key = True, unique = True)
     dionysos_username = models.CharField(max_length = 15, unique = True)
     dionysos_password = models.CharField(max_length = 30)
     eclass_username = models.CharField(max_length = 30, null = True, blank = True)
     eclass_password = models.CharField(max_length = 30, null = True, blank = True)
-    eclass_lessons = models.TextField(null = True, blank = True)
+    eclass_lessons = models.ManyToManyField(EclassLessons)
     introduction_year = models.CharField(max_length = 5)
     registration_number = models.CharField(max_length = 8)
     school = models.ForeignKey(Departments)
     semester = models.CharField(max_length = 2)
     webmail_username = models.CharField(max_length = 30, null = True, blank = True)
     webmail_password = models.CharField(max_length = 30, null = True, blank = True)
-    teacher_announcements = models.TextField(null = True, blank = True)
-    other_announcements = models.TextField(null = True, blank = True)
+    teacher_announcements = models.ManyToManyField(Teachers)
+    other_announcements = models.ManyToManyField(Websites)
     declaration = models.TextField(null = True, blank = True)
     grades = models.TextField(null = True, blank = True)
 
