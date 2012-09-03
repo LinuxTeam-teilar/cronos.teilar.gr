aaec2e20d465e1bacf577b009e0886d08b91d313
Theo Chatzimichos <tampakrap@linuxteam.teilar.gr>
2012-09-04 01:44:09 +0200

    Enable email for departments, although it is not fetched by the cronjob yet

--- cronos/teilar/models.py
+++ cronos/teilar/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Departments(models.Model):
     url = models.URLField(unique = True)
     name = models.CharField(max_length = 255)
-#    email = models.EmailField(null = True)
+    email = models.EmailField(null = True)
     is_active = models.BooleanField(default = True)
 
     def __unicode__(self):
