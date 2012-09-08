b2f5ed2f991bd045e7d9e3769248a0367941d1f6
Theo Chatzimichos <tampakrap@linuxteam.teilar.gr>
2012-09-08 03:47:08 +0200

    small update the get_all_real_accounts script due to OneToOne relation in userprofile table

--- cronos/teilar/management/commands/get_all_real_accounts.py
+++ cronos/teilar/management/commands/get_all_real_accounts.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
 
-from cronos.accounts.models import UserProfile
 from cronos.common.encryption import decrypt_password
+from django.contrib.auth.models import User
 from django.core.management.base import BaseCommand
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
         all_students = {}
-        all_students_q = UserProfile.objects.filter(is_active = True)
+        all_students_q = User.objects.filter(is_active = True)
         for student in all_students_q:
-            all_students[student.dionysos_username] = decrypt_password(student.dionysos_password)
+            all_students[student.get_profile().dionysos_username] = decrypt_password(student.get_profile().dionysos_password)
 
         self.stdout.write('all_real_accounts = %s\n' % str(all_students))
