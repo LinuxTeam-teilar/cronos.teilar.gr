848858b0e3bb70f0c9f289ba795fc90f085701db
Theo Chatzimichos <tampakrap@linuxteam.teilar.gr>
2012-09-16 03:22:36 +0200

    Print the following eclass lessons (and update the view of teachers to have the same style)

--- cronos/teilar/views.py
+++ cronos/teilar/views.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from cronos.teilar.forms import LibraryForm
-from cronos.teilar.models import Teachers
+from cronos.teilar.models import Teachers, EclassLessons
 from cronos import teilar_anon_login
 from bs4 import BeautifulSoup
 from django.contrib.auth.decorators import login_required
@@ -15,7 +15,7 @@ def teachers(request):
     '''
     teachers = Teachers.objects.filter(is_active = True).order_by('name')
     for teacher in teachers:
-        teacher.url = teacher.url.split('=')[1]
+        teacher.urlid = teacher.url.split('=')[1]
     return render_to_response('teachers.html', {
             'teachers': teachers,
         }, context_instance = RequestContext(request))
@@ -129,47 +129,7 @@ def dionysos(request):
 
 @login_required
 def eclass(request):
-    try:
-        output = eclass_login(request.user.get_profile().eclass_username, decryptPassword(request.user.get_profile().eclass_password))
-        soup = BeautifulSoup(output)
-
-        soup1 = BeautifulSoup(str(soup.findAll('tr', 'odd')[3]))
-        deadlines = []
-        i = 0
-        for item in soup1.findAll('li', 'category'):
-            lesson = item.contents[0]
-            title = soup1.findAll('a', 'square_bullet2')[i].contents[0].contents[0]
-            date = soup1.findAll('p', 'content_pos')[i].b.contents[0]
-            status = soup1.findAll('span')[i].contents[0]
-            deadlines.append([lesson, title, date, status])
-            i += 1
-    except:
-        deadlines = None
-
-    try:
-        soup1 = BeautifulSoup(str(soup.findAll('tr', 'odd')[4]))
-        documents = []
-        i = 0
-        j = 0
-        for item in soup1.findAll('li'):
-            if item in soup1.findAll('li', 'category'):
-                lesson = item.contents[0]
-                i += 1
-            else:
-                title = soup1.findAll('a', 'square_bullet2')[j].contents[0].contents[0].split(' - (')[0]
-                date = soup1.findAll('a', 'square_bullet2')[j].contents[0].contents[0].split(' - (')[1][:-1]
-                documents.append([lesson, title, date])
-                j += 1
-    except:
-        documents = None
-
-    eclass_lessons = []
-    for item in Id.objects.filter(urlid__in = request.user.get_profile().eclass_lessons.split(',')):
-        eclass_lessons.append([item.urlid.strip(), item.name])
-
+    eclass_lessons = EclassLessons.objects.all().order_by('faculty', '-ltype', 'name')
     return render_to_response('eclass.html', {
-            'headers': ['ΤΑ ΜΑΘΗΜΑΤΑ ΜΟΥ', 'ΟΙ ΔΙΟΡΙΕΣ ΜΟΥ', 'ΤΑ ΤΕΛΕΥΤΑΙΑ ΜΟΥ ΕΓΓΡΑΦΑ'],
             'eclass_lessons': eclass_lessons,
-            'deadlines': deadlines,
-            'documents': documents,
         }, context_instance = RequestContext(request))
