228e7247a35dd6262f5bc5ed4e29afabcb142597
Theo Chatzimichos <tampakrap@linuxteam.teilar.gr>
2012-09-18 01:14:37 +0200

    Improve log_extra_data, so it can have only one argument, check if it is a string or a request, and pass the appropriate data to the handler

--- cronos/common/log.py
+++ cronos/common/log.py
@@ -2,19 +2,24 @@
 
 from django.conf import settings
 
-def log_extra_data(id_name = None, request = None):
+def log_extra_data(additional = None):
     '''
     Extra data needed by the custom formatter
-    All values default to None
-    It provides three data: client_ip, username and cronjob name
-    Username can be passed directly as argument, or it can be retrieved by
-    either the request var or the form
+    * If the additional argument is a string or unicode, then its value is printed
+    in the log.
+    * If the additiona argument is the request, then from the request it
+    prints the client IP and username if applicable.
     '''
     log_extra_data = {
-        'client_ip': request.META.get('REMOTE_ADDR','None') if request else '',
-        'id_name': id_name if id_name else '',
+        'client_ip': '',
+        'id_name': '',
     }
-    if not id_name:
+    if type(additional) == str or type(additional) == unicode:
+        log_extra_data['id_name'] = additional
+    else:
+        request = additional
+        if request.META:
+            log_extra_data['client_ip'] = request.META.get('REMOTE_ADDR', 'None')
         try:
             if request.user.is_authenticated():
                 '''
