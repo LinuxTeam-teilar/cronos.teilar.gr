1f356e2769b37a150596ba4d12454a28438ad9cb
Theo Chatzimichos <tampakrap@linuxteam.teilar.gr>
2012-09-21 00:55:59 +0200

    exclude admin usernames from the test fixtures

--- cronos/teilar/management/commands/get_all_real_accounts.py
+++ cronos/teilar/management/commands/get_all_real_accounts.py
@@ -1,13 +1,15 @@
 # -*- coding: utf-8 -*-
 
 from cronos.common.encryption import decrypt_password
+from cronos.common.get_admins import get_admins_usernames
 from django.contrib.auth.models import User
 from django.core.management.base import BaseCommand
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
         all_students = {}
-        all_students_q = User.objects.filter(is_active = True)
+        admins = get_admins_usernames()
+        all_students_q = User.objects.filter(is_active = True).exclude(username_in = admins)
         for student in all_students_q:
             all_students[student.get_profile().dionysos_username] = decrypt_password(student.get_profile().dionysos_password)
 
