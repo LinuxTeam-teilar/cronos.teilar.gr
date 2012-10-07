bd1a6073e63a8c6d37534e3dd9636ab385f1680f
Theo Chatzimichos <tampakrap@linuxteam.teilar.gr>
2012-10-07 22:20:37 +0300

    fix url

--- cronos/teilar/management/commands/create_rss_feed.py
+++ cronos/teilar/management/commands/create_rss_feed.py
@@ -111,7 +111,7 @@ class Command(BaseCommand):
         custom_rss = self.initialize_rss_file()
         for i in reversed(range(17)):
             page_no = i+1
-            output = teilar_anon_login('http://www.teilar.gr/tmimatanews.php?cid=&page%s' % page_no)
+            output = teilar_anon_login('http://www.teilar.gr/tmimatanews.php?cid=&page=%s' % page_no)
             soup = BeautifulSoup(output)
             try:
                 announcements_all = soup.find_all('table')[17].find_all('a', 'BlackText11')
